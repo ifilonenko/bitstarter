cdff184243a217ff9e7d509caae4fd75afdfce59
Ilan Filonenko <ilan.fil@gmail.com>
2013-07-06 22:52:38 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -3,10 +3,10 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-    var fs = require ("fs");
-    var buffer = new Buffer (500);
-        response.send(buffer.toString("utf-8", fs.readFileSync('index.html')));
-});
+    //response.send('Hello World 2!');
+    buffer = fs.readFileSync("index.html").toString("utf8");
+    response.send(buffer);
+    //console.log(fs.readFileSync('index.html', 'utf8});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
