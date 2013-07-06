cd9602fa3cdee331b8de5ac40ef48534248cae7b
Ilan Filonenko <ilan.fil@gmail.com>
2013-07-06 22:56:23 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -2,6 +2,10 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+
+var buffer = new Buffer(256);
+
 app.get('/', function(request, response) {
     //response.send('Hello World 2!');
     buffer = fs.readFileSync("index.html").toString("utf8");
